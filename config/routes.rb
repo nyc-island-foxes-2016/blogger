64d3ca322ea6bb07e8aa11ce23aad24efcccbbb2
Steven Cassidy <steven@mumsnet.com>
2016-01-19 14:24:42 -0500

    Add a posts#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index]
+  resources :posts, only: [:index, :show]
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create]
   get 'register' => 'users#new'
+  root 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
