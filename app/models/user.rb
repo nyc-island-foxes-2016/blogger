576935f0611b4b2fd846e412e66bf4cad451b652
Steven Cassidy <steven@mumsnet.com>
2016-01-19 14:31:09 -0500

    Add comments model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, :username, presence: true
   has_many :posts
+  has_many :comments
+  has_many :received_comments, through: :posts, source: :comments
 end
